908ef8741c7003d94de8cf0bfb296dd9d163dce1
cr57.jayson <cr57.jayson@enfrausaph.com>
2019-02-07 21:34:35 +0800

    Done, Desktop View Only

--- script.js
+++ script.js
@@ -1,30 +1,4 @@
-// var slideIndex = 1;
-// showSlides(slideIndex);
-
-// function plusSlides(n) {
-//   showSlides(slideIndex += n);
-// }
-
-// function currentSlide(n) {
-//   showSlides(slideIndex = n);
-// }
-
-// function showSlides(n) {
-//   var i;
-//   var slides = document.getElementsByClassName("mySlides");
-//   var dots = document.getElementsByClassName("dot");
-//   if (n > slides.length) {slideIndex = 1}
-//   if (n < 1) {slideIndex = slides.length}
-//   for (i = 0; i < slides.length; i++) {
-//       slides[i].style.display = "none";
-//   }
-//   for (i = 0; i < dots.length; i++) {
-//       dots[i].className = dots[i].className.replace(" active", "");
-//   }
-//   slides[slideIndex-1].style.display = "block";
-//   dots[slideIndex-1].className += " active";
-// }
-$(window).on('load',function(){
+$(window).on("load", function() {
   let part2 = $(".part-2").prop("style");
   let slides = $(".product-roll").prop("style");
   let slideImage = $(".product-image");
@@ -32,7 +6,7 @@ $(window).on('load',function(){
   let details = $(".details-item-container").prop("style");
   let detailItem = $(".details-item");
   let features = $(".features-image");
-  let sizes = $('.product-sizes span');
+  let sizes = $(".product-sizes span");
   let dots = $(".dot-border");
   let activeItem;
   rollImage(0);
@@ -63,15 +37,15 @@ $(window).on('load',function(){
   }
   function rollImage(i) {
     imageShow();
-  //SEED
+    //SEED
     if (i === 1) {
       slides.transform = "translateY(-220px)";
-      details.transform = "translateY(-58px)";
+      details.transform = "translateY(-60px)";
       features[0].style.opacity = 1;
       features[1].style.opacity = 0;
       features[2].style.opacity = 0;
       sizes[0].style.opacity = 1;
-      sizes[0].textContent ="8" + '"';
+      sizes[0].textContent = "8" + '"';
       sizes[0].style.padding = "7px";
       sizes[1].style.opacity = 1;
       sizes[2].style.opacity = 1;
@@ -79,13 +53,13 @@ $(window).on('load',function(){
       dots[0].style.opacity = 1;
       dots[1].style.opacity = 1;
       dots[2].style.opacity = 1;
-      dots.removeClass('dot-iris');
+      dots.removeClass("dot-iris");
       dots[1].style.border = "1px solid";
       activeItem = 1;
       linkActive(1, "#seed");
-      $("#arrowUp").prop('style').opacity = 1;
-      $("#arrowDown").prop('style').opacity = 1;
-  //EDGE
+      $("#arrowUp").prop("style").opacity = 1;
+      $("#arrowDown").prop("style").opacity = 1;
+      //EDGE
     } else if (i === 2) {
       slides.transform = "translateY(-480px)";
       details.transform = "translateY(-110px)";
@@ -93,7 +67,7 @@ $(window).on('load',function(){
       features[1].style.opacity = 1;
       features[2].style.opacity = 1;
       sizes[0].style.opacity = 1;
-      sizes[0].textContent ="8" + '"';
+      sizes[0].textContent = "8" + '"';
       sizes[0].style.padding = "7px";
       sizes[1].style.opacity = 0;
       sizes[2].style.opacity = 0;
@@ -101,13 +75,13 @@ $(window).on('load',function(){
       dots[0].style.opacity = 0;
       dots[1].style.opacity = 1;
       dots[2].style.opacity = 0;
-      dots.removeClass('dot-iris');
+      dots.removeClass("dot-iris");
       dots[1].style.border = "1px solid";
       activeItem = 2;
       linkActive(2, "#edge");
-      $("#arrowUp").prop('style').opacity = 1;
-      $("#arrowDown").prop('style').opacity = 1;
-  //ORIGINAL
+      $("#arrowUp").prop("style").opacity = 1;
+      $("#arrowDown").prop("style").opacity = 1;
+      //ORIGINAL
     } else if (i === 3) {
       slides.transform = "translateY(-800px)";
       details.transform = "translateY(-160px)";
@@ -116,47 +90,46 @@ $(window).on('load',function(){
       features[2].style.opacity = 1;
       sizes[0].style.opacity = 1;
       sizes[0].style.padding = "7px 3px";
-      sizes[0].textContent ="15" + '"';
+      sizes[0].textContent = "15" + '"';
       sizes[1].style.opacity = 0;
       sizes[2].style.opacity = 0;
       part2.background = "linear-gradient(#87d6c4, #b4dfed)";
       dots[0].style.opacity = 0;
       dots[1].style.opacity = 1;
       dots[2].style.opacity = 0;
-      dots.removeClass('dot-iris');
+      dots.removeClass("dot-iris");
       dots[1].style.border = "1px solid";
       activeItem = 3;
       linkActive(3, "#original");
-      $("#arrowDown").prop('style').opacity = 0;
-      $("#arrowUp").prop('style').opacity = 1;
-  //IRIS
+      $("#arrowDown").prop("style").opacity = 0;
+      $("#arrowUp").prop("style").opacity = 1;
+      //IRIS
     } else {
       slides.transform = "translateY(40px)";
-      details.transform = "translateY(-5px)";
+      details.transform = "translateY(0px)";
       features[0].style.opacity = 1;
       features[1].style.opacity = 0;
       features[2].style.opacity = 0;
       sizes[0].style.opacity = 1;
-      sizes[0].textContent ="8" + '"';
+      sizes[0].textContent = "8" + '"';
       sizes[0].style.padding = "7px";
       sizes[1].style.opacity = 0;
       sizes[2].style.opacity = 0;
       part2.background = "linear-gradient(#feb5b6, #feddd4)";
-      dots.addClass('dot-iris');
+      dots.addClass("dot-iris");
       dots[1].style.border = "0px solid";
       activeItem = 0;
       linkActive(0, "#iris");
-      $("#arrowUp").prop('style').opacity = 0;
-      $("#arrowDown").prop('style').opacity = 1;
+      $("#arrowUp").prop("style").opacity = 0;
+      $("#arrowDown").prop("style").opacity = 1;
     }
     setTimeout(function() {
       imageHide(i);
     }, 500);
   }
 
-//3DOTS
-  
-  
+  //3DOTS
+
   //SideNav
   function linkActive(num, id) {
     for (i = 0; i < sideNav.length; i++) {
@@ -166,31 +139,25 @@ $(window).on('load',function(){
     }
     $(id).addClass("link-active");
   }
-  
+
   $("#iris").on("click", function() {
     rollImage(0);
-    
   });
   $("#seed").on("click", function() {
     rollImage(1);
-   
   });
   $("#edge").on("click", function() {
     rollImage(2);
-    
   });
   $("#original").on("click", function() {
     rollImage(3);
-    
   });
   //Arrow Nav
-$("#arrowUp").on("click", function() {
-  rollImage(activeItem === 0 ? 0 : activeItem - 1);
+  $("#arrowUp").on("click", function() {
+    rollImage(activeItem === 0 ? 0 : activeItem - 1);
+  });
+
+  $("#arrowDown").on("click", function() {
+    rollImage(activeItem === 3 ? 3 : activeItem + 1);
+  });
 });
-
-$("#arrowDown").on("click", function() {
-  rollImage(activeItem === 3 ? 3 : activeItem + 1);
-});
-})
-
-
